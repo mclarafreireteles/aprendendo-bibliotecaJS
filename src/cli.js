61ecce0cccbef579a124034c47b269dbb437808a
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2023-10-08 13:55:32 -0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,19 @@
 import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from './teste.js';
 
 const caminho = process.argv;
+
 pegaArquivo(caminho[2]);
 // console.log(caminho); // retorna um array c o caminho absoluto ate o executavel do node e ate esse codigo
 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de links: '), resultado);
+async function processaTexto(argumentos){
+    if (fs.linkSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        console.log(chalk.yellow('lista de links: '), resultado);
+    } else if (fs.linkSync(caminho).isDirectory()) {
+        
+    }
 }
 
 processaTexto(caminho);
